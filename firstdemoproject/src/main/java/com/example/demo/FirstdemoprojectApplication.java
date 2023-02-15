73241f9600c29582602800e329aff2e6b4056eba
Debadyuti <debadyuti.karmakar@gmail.com>
2023-02-15 20:42:29 +0530

    First Hello World project Spring Boot

---
+++ firstdemoproject/src/main/java/com/example/demo/FirstdemoprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstdemoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstdemoprojectApplication.class, args);
+	}
+
+}
