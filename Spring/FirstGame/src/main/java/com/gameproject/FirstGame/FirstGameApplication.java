a5b635d9e6167004c74f4a4c78162cd7f2d032c3
Debadyuti <debadyuti.karmakar@gmail.com>
2023-02-16 21:08:27 +0530

    Create new spring project FirstGame

---
+++ Spring/FirstGame/src/main/java/com/gameproject/FirstGame/FirstGameApplication.java
@@ -0,0 +1,13 @@
+package com.gameproject.FirstGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstGameApplication.class, args);
+	}
+
+}
