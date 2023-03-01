bd20fcdb43c55917810a14bf10b3c1cf3968ba57
Debadyuti <debadyuti.karmakar@gmail.com>
2023-03-02 00:59:32 +0530

    Add more using record

--- Spring/FirstGame/src/main/java/com/gameproject/FirstGame/App02HelloWorldSpring.java
+++ Spring/FirstGame/src/main/java/com/gameproject/FirstGame/App02HelloWorldSpring.java
@@ -16,11 +16,15 @@ public class App02HelloWorldSpring {
 				new AnnotationConfigApplicationContext(HelloWorldConfigaration.class);
 		//2. Configure the things that we want spring to manage- @Configaration
 		
-		//HelloWorldConfigaration - @Configaration
+		//HelloWorldConfigaration - @ConfigarationSystem.out.println(context.getBean("Name"));
+		
 		//name - @Bean
 		
 		//3. Retrieving Beans managed by Spring
 		System.out.println(context.getBean("Name"));
+		System.out.println(context.getBean("Age"));
+		System.out.println(context.getBean("person"));
+		System.out.println(context.getBean("address"));
 		
 		
 	}
--- Spring/FirstGame/src/main/java/com/gameproject/FirstGame/HelloWorldConfigaration.java
+++ Spring/FirstGame/src/main/java/com/gameproject/FirstGame/HelloWorldConfigaration.java
@@ -3,6 +3,10 @@ package com.gameproject.FirstGame;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age) {};
+
+record Address(String firstline, String city) {};
+
 @Configuration
 public class HelloWorldConfigaration {
 	
@@ -11,5 +15,19 @@ public class HelloWorldConfigaration {
 		return "Debadyuti";
 		
 	}
-
+	
+	@Bean
+	public int Age() {
+		return 22;
+	}
+	
+	@Bean
+	public Person person() {
+		return new Person("Ram",20);
+	}
+	
+	@Bean
+	public Address address() {
+		return new Address("N.S Road","Mankundu");
+	}
 }
