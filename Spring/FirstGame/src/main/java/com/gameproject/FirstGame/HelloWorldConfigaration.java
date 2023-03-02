0459d2ce0e9ed152cd9d86ccf8381938ba59abb3
Debadyuti <debadyuti.karmakar@gmail.com>
2023-03-03 01:27:16 +0530

    Auto wiring

--- Spring/FirstGame/src/main/java/com/gameproject/FirstGame/HelloWorldConfigaration.java
+++ Spring/FirstGame/src/main/java/com/gameproject/FirstGame/HelloWorldConfigaration.java
@@ -1,7 +1,9 @@
 package com.gameproject.FirstGame;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 record Person(String name, int age, Address address) {};
 
@@ -32,13 +34,33 @@ public class HelloWorldConfigaration {
 	}
 	
 	@Bean
-	public Person person3Parameters(String Name,int Age, Address address2) {// Name,Age,address2
-		return new Person(Name,Age,address2
+	public Person person3Parameters(String Name,int Age, Address address3) {// Name,Age,address2
+		return new Person(Name,Age,address3
+				);
+	}
+	
+	@Bean
+	@Primary
+	public Person person4Parameters(String Name,int Age, Address address) {// Name,Age,address2
+		return new Person(Name,Age,address
+				);
+	}
+	
+	@Bean
+	public Person person5QualifierParameters(String Name,int Age, @Qualifier("address3qualifier") Address address) {// Name,Age,address2
+		return new Person(Name,Age,address
 				);
 	}
 	
 	@Bean(name = "address2")
+	@Primary
 	public Address address() {
 		return new Address("N.S Road","Mankundu");
+	}
+	
+	@Bean(name = "address3")
+	@Qualifier("address3qualifier")
+	public Address address3() {
+		return new Address("Palpara","Mankundu");
 	}
 }
