44ca98b749d0327d8f459b3603c9fb4ac23dde3f
Debadyuti <debadyuti.karmakar@gmail.com>
2023-03-04 00:19:09 +0530

    Using constructor injection spring framework

--- Spring/Game02/src/main/java/com/gameproject/FirstGame/examples/a1/DepInjectionLauncherApplication.java
+++ Spring/Game02/src/main/java/com/gameproject/FirstGame/examples/a1/DepInjectionLauncherApplication.java
@@ -10,25 +10,29 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Component
-class YourBusinessClass{
-	//@Autowired
+class YourBusinessClass {
+	// @Autowired
 	Dependency1 dependency1;
-	//@Autowired
+	// @Autowired
 	Dependency2 dependency2;
-	
-	
+
 	@Autowired
-	public void setDependency1(Dependency1 dependency1) {
+	public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+		super();
 		this.dependency1 = dependency1;
-	}
-
-
-	@Autowired
-	public void setDependency2(Dependency2 dependency2) {
 		this.dependency2 = dependency2;
 	}
 
-
+//	@Autowired
+//	public void setDependency1(Dependency1 dependency1) {
+//		this.dependency1 = dependency1;
+//	}
+//
+//
+//	@Autowired
+//	public void setDependency2(Dependency2 dependency2) {
+//		this.dependency2 = dependency2;
+//	}
 
 	public String toString() {
 		return " using " + dependency1 + " and " + dependency2;
@@ -36,16 +40,14 @@ class YourBusinessClass{
 }
 
 @Component
-class Dependency1{
-	
+class Dependency1 {
+
 }
 
 @Component
-class Dependency2{
-	
+class Dependency2 {
+
 }
-
-
 
 @Configuration
 @ComponentScan
@@ -55,10 +57,9 @@ public class DepInjectionLauncherApplication {
 		// TODO Auto-generated method stub
 
 		try (var context = new AnnotationConfigApplicationContext(DepInjectionLauncherApplication.class)) {
-			
-			Arrays.stream(context.getBeanDefinitionNames())
-			.forEach(System.out::println);
-			
+
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
 			System.out.println(context.getBean(YourBusinessClass.class));
 		}
 	}
