afd5632bea9354f9b59991a9e089b706c6279feb
Debadyuti <debadyuti.karmakar@gmail.com>
2023-03-15 22:11:49 +0530

    insert data using spring

---
+++ springboot/learn-jpa-and-hibernate/src/main/java/com/springboot/ex/learnjpaandhibernate/course/jdbc/CoursejdbcCommandLineRunner.java
@@ -0,0 +1,19 @@
+package com.springboot.ex.learnjpaandhibernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CoursejdbcCommandLineRunner implements CommandLineRunner{
+	@Autowired
+	private CoursejdbcRepository repository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		repository.insert();
+	}
+	
+	
+
+}
