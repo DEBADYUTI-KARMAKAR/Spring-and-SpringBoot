205ff038034fa41ae6df074de6ca35b680a7d30a
Debadyuti <debadyuti.karmakar@gmail.com>
2023-03-16 22:06:02 +0530

    custom spring data jpa method

--- springboot/learn-jpa-and-hibernate/src/main/java/com/springboot/ex/learnjpaandhibernate/course/springdatajpa/CourseSpringDataJpaRepository.java
+++ springboot/learn-jpa-and-hibernate/src/main/java/com/springboot/ex/learnjpaandhibernate/course/springdatajpa/CourseSpringDataJpaRepository.java
@@ -1,10 +1,16 @@
 package com.springboot.ex.learnjpaandhibernate.course.springdatajpa;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springboot.ex.learnjpaandhibernate.course.Course;
 
 public interface CourseSpringDataJpaRepository extends JpaRepository<Course, Long> {
 
+	List<Course> findByAuthor(String author);
+	
+	List<Course> findByName(String name);
+	
 	
 }
